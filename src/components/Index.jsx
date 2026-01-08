f9ca98872708bb6f8474fc4e28ab7af305fdc598
SantiG912 <santiagoagrandoli03@gmail.com>
2026-01-08 18:45:27 -0300

    indice con ligas y partidos destacados

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import useFetch from '../api/useFetch'
+import { Link } from 'react-router-dom';
+import Matches from './Matches';
+
+const FEATURED_COMPETITIONS = [2021, 2001, 2013, 2014, 2019];
 
 export default function Index() {
+  const {data, loading, error} = useFetch("competitions");
+
+  if(loading || data === null) return <p>Cargando datos...</p>
+  if(error) return <p>Error cargando los datos</p>
+  if(!data?.competitions) return null
+
+  const featuredCompetitions = data.competitions.filter(comp =>
+    FEATURED_COMPETITIONS.includes(comp.id)
+  );
+
   return (
-    <div>Index</div>
+    <section className="index-container">
+      <section className="featured-competitions">
+        <section className="competition-grid">
+          {featuredCompetitions.map(comp => (
+            <Link
+              key={comp.id}
+              to={`/competitions/${comp.id}`}
+              className="competition-card"
+            >
+              <img
+              src={comp.emblem}
+              alt={comp.name}
+              />
+              <h3>{comp.name}</h3>
+            </Link>
+          ))}
+        </section>
+      </section>
+
+      <section className="today-matches">
+        <h3>Hoy:</h3>
+        <Matches/>
+      </section>
+    </section>
   )
 }
--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -2,65 +2,54 @@ import React from 'react'
 import useFetch from '../api/useFetch';
 import { formatDate } from '../api/formatDate';
 
-export default function Matches({competitionId}) {
-    const {
-        data: matchData, 
-        loading: matchLoading, 
-        error: matchError
-    } = useFetch(`competitions/${competitionId}/matches`);
+export default function Matches() {
+  const today = new Date().toISOString().split("T")[0];
 
-    if(matchError) return <p>Error cargando los partidos: {matchError}</p>
-    if(matchLoading) return <p>Cargando partidos...</p>
-    if(!matchData) return <p>No hay datos.</p>
-    console.log(matchData);
+  const { data, loading, error } = useFetch(
+    `matches?dateFrom=${today}&dateTo=${today}`
+  );
 
-    const matchesByMatchday = matchData?.matches?.reduce((acc, match) => {
-        const matchday = match.matchday;
+  if(loading || data === null) return <p>Cargando partidos...</p>;
+  if(error) return <p>Error cargando partidos</p>;
+  if(!data?.matches || data.matches.length === 0) {
+    return <p>No hay partidos hoy</p>;
+  }
 
-        if(!acc[matchday]){
-            acc[matchday] = [];
-        }
-        acc[matchday].push(match);
-        return acc;
-    }, {});
-    
-    return (
-        matchData && (
-            <section className="matches-container">
-                {Object.entries(matchesByMatchday).map(([matchday, matches]) => (
-                    <section key={matchday} className="matchday-section">
-                        <h3 className="matchday-title">
-                            Jornada {matchday}
-                        </h3>
-                        <section className="matchday-table">
-                            {matches.map(match => (
-                                <section key={match.id} className="matches">
-                                    <img 
-                                    src={match.homeTeam.crest}
-                                    alt={match.homeTeam.shortName}
-                                    />
-                                    <span className="matchday-teams">
-                                        {match.homeTeam.name}
-                                        {" "}
-                                        {match.score.fullTime.home}
-                                        {" - "}
-                                        {match.score.fullTime.away}
-                                        {" "}
-                                        {match.awayTeam.name}
-                                    </span>
-                                    <img 
-                                    src={match.awayTeam.crest}
-                                    alt={match.awayTeam.shortName}
-                                    />
-                                    <span className="matchday-date">
-                                        {formatDate(match.utcDate)}
-                                    </span>
-                                </section>
-                            ))}
-                        </section>
-                    </section>
-                ))}
+  return (
+    <section className="matches-container">
+      <section className="matchday-table">
+
+          {data.matches.map(match => (
+
+            <section key={match.id} className="matches">
+              
+              <img
+                src={match.homeTeam.crest}
+                alt={match.homeTeam.shortName}
+              />
+  
+              <span className="matchday-teams">
+                {match.homeTeam.name}{" "}
+                {match.score.fullTime.home}
+                {" - "}
+                {match.score.fullTime.away}{" "}
+                {match.awayTeam.name}
+              </span>
+  
+              <img
+                src={match.awayTeam.crest}
+                alt={match.awayTeam.shortName}
+              />
+  
+              <span className="matchday-date">
+                {formatDate(match.utcDate)}
+              </span>
             </section>
-        )
-    );
+  
+          ))}
+      
+      </section>
+    </section>
+  );
 }
+
